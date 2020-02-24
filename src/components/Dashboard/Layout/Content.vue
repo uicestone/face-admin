7eafd39c3cc4d23e81d5a2ca152b79019ab37698
Uice Lu <uicestone@gmail.com>
2020-02-24 23:51:22 +0800

    disable router transition

--- src/components/Dashboard/Layout/Content.vue
+++ src/components/Dashboard/Layout/Content.vue
@@ -1,30 +1,25 @@
 <template>
   <div class="content">
-    <transition name="fade" mode="out-in">
-      <keep-alive>
-        <router-view v-if="$route.meta.keepAlive" :key="$route.fullPath"></router-view>
-      </keep-alive>
-    </transition>
-    <transition name="fade" mode="out-in">
-      <router-view v-if="!$route.meta.keepAlive" :key="$route.fullPath"></router-view>
-    </transition>
+    <keep-alive>
+      <router-view v-if="$route.meta.keepAlive" :key="$route.fullPath"></router-view>
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive" :key="$route.fullPath"></router-view>
   </div>
 </template>
 <script>
-  export default {}
+export default {};
 </script>
 <style lang="scss" scoped>
-  
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity 0.3s;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
 
-  .fade-enter,
+.fade-enter,
   .fade-leave-to
     /* .fade-leave-active in <2.1.8 */
 
-  {
-    opacity: 0
-  }
+ {
+  opacity: 0;
+}
 </style>
