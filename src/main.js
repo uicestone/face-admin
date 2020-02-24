776c6de02735fafc70490453a1f9309e5575254f
Uice Lu <uicestone@gmail.com>
2020-02-25 00:42:07 +0800

    element-ui css

--- src/main.js
+++ src/main.js
@@ -1,39 +1,40 @@
-import Vue from 'vue'
-import './pollyfills'
-import VueNotify from 'vue-notifyjs'
-import VeeValidate from 'vee-validate'
-import lang from 'element-ui/lib/locale/lang/zh-CN'
-import locale from 'element-ui/lib/locale'
-import App from './App.vue'
-import moment from 'moment'
-import router from './router/router'
+import Vue from "vue";
+import "./pollyfills";
+import VueNotify from "vue-notifyjs";
+import VeeValidate from "vee-validate";
+import lang from "element-ui/lib/locale/lang/zh-CN";
+import locale from "element-ui/lib/locale";
+import App from "./App.vue";
+import moment from "moment";
+import router from "./router/router";
 
 // Plugins
-import GlobalComponents from './globalComponents'
-import GlobalDirectives from './globalDirectives'
-import SideBar from './components/UIComponents/SidebarPlugin'
+import GlobalComponents from "./globalComponents";
+import GlobalDirectives from "./globalDirectives";
+import SideBar from "./components/UIComponents/SidebarPlugin";
 
 // library imports
 
-import './assets/sass/paper-dashboard.scss'
-import './assets/sass/demo.scss'
+import "element-ui/lib/theme-chalk/index.css";
+import "./assets/sass/paper-dashboard.scss";
+import "./assets/sass/demo.scss";
 
-import sidebarLinks from './sidebarLinks'
-import './registerServiceWorker'
-import { createProvider } from './vue-apollo'
+import sidebarLinks from "./sidebarLinks";
+import "./registerServiceWorker";
+import { createProvider } from "./vue-apollo";
 // plugin setup
-Vue.use(GlobalDirectives)
-Vue.use(GlobalComponents)
-Vue.use(VueNotify)
-Vue.use(SideBar, {sidebarLinks: sidebarLinks})
-Vue.use(VeeValidate, {fieldsBagName: 'veeFields'})
-locale.use(lang)
-moment.locale('zh-CN')
+Vue.use(GlobalDirectives);
+Vue.use(GlobalComponents);
+Vue.use(VueNotify);
+Vue.use(SideBar, { sidebarLinks: sidebarLinks });
+Vue.use(VeeValidate, { fieldsBagName: "veeFields" });
+locale.use(lang);
+moment.locale("zh-CN");
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App),
   apolloProvider: createProvider(),
   router
-})
+});
